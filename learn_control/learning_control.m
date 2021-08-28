5761e41f60c0eba299b68b76d7b5cead71351452
Alados5 <adria.alados@gmail.com>
2021-08-28 06:47:25 +0200

    Continued Exps3 RTM Analysis (4/24)

--- learn_control/learning_control.m
+++ learn_control/learning_control.m
@@ -3,7 +3,7 @@ close all; clc; clear;
 %% Initialization
 ExpSet = 3;
 SimType = 'RTM'; %LIN, NL, RTM
-ExpNote = '_Det_W1_RwdE_Qa_u';
+ExpNote = '_Det_W1_RwdE_Qk_u';
 NTraj = 6;
 Ts = 0.020;
 Hp = 25;
@@ -17,11 +17,11 @@ ubound  = 50*1e-3;  % (Enough Displ.)
 gbound  = 0;        % (Eq. Constraint)
 
 opt_Du  = 0;  % 0=u,      1=Du
-opt_Qa  = 1;  % 0=Qk,     1=Qa*Qk
+opt_Qa  = 0;  % 0=Qk,     1=Qa*Qk
 opt_Rwd = 1;  % 1=RMSE,   2=Tov,           3=RMSE+Tov
 opt_Wgh = 1;  % 1=[q r],  2=[qx qy qz r],  3=[qx qy qz k]
 
-e0 = 0;
+e0 = 15;
 minRwd = -10;
 NSamples = 10;
 NEpochs = 5;
@@ -427,6 +427,19 @@ if(Plot2DTraj==1)
     Lgnd4.Position(2) = 0.05;
 
 end
+
+% Distribution plots
+%{
+foo = mvnrnd(mw,Sw,10);
+foo1 = foo./max(foo,[],2);
+scatter(foo1(:,1),foo1(:,2),'ob','filled')
+grid on; box on; axis equal;
+hold on
+plot([1,1,0],[0,1,1],'--k')
+xlim([0 1.1])
+ylim([0 1.1])
+hold off
+%}
 
 
 %% Update LUT for Parameters
